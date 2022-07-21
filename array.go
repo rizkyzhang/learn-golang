9b2e0e0a5e6cb27af7643f81c0d7ae736be82717
rizkyzhang <rizkyzhangdev@gmail.com>
2022-07-21 11:56:10 +0700

    feat: update array.go

--- array.go
+++ array.go
@@ -9,20 +9,28 @@ func learnArray() {
 	fmt.Println(nums)
 	fmt.Println(evenNums)
 
+	// Define array with make keyword
 	oddNums := make([]int, 2)
 	oddNums[0] = 1
 	oddNums[1] = 3
 
-	fmt.Println("old oddNums:", oddNums)
+	fmt.Println("original oddNums:", oddNums)
 
+	// Array looping
 	for _, num := range nums {
 		fmt.Println(num)
 	}
 
+	// Array reference test
 	oddNumsCopy := oddNums
 	oddNumsCopy[0] = 1
 	oddNumsCopy[1] = 5
 
-	fmt.Println("new oddNums:", oddNums)
+	fmt.Println("original oddNums:", oddNums)
 	fmt.Println("copy oddNums:", oddNumsCopy)
+
+	// Multi dimensional array
+	matrix := [2][3]int{{1, 2, 3}, {1, 2, 3}}
+
+	fmt.Println(matrix)
 }
